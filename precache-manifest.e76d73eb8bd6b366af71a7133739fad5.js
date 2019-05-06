04c85973bef27348d1990d7a6ea66617b8ff35a6
Jannik Fuhrmann <peachvizzle@gmail.com>
2019-05-06 13:08:20 +0200

    Updates

---
+++ precache-manifest.e76d73eb8bd6b366af71a7133739fad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "82ad2ec522b6e8b9e94b",
+    "url": "/contactmanager/static/js/main.d928c70c.chunk.js"
+  },
+  {
+    "revision": "01e89a929d4ec8ff44a2",
+    "url": "/contactmanager/static/js/2.11771582.chunk.js"
+  },
+  {
+    "revision": "82ad2ec522b6e8b9e94b",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "01e89a929d4ec8ff44a2",
+    "url": "/contactmanager/static/css/2.f404029e.chunk.css"
+  },
+  {
+    "revision": "6e04ad801ac2c8fa84bb2e998b65906b",
+    "url": "/contactmanager/index.html"
+  }
+];
